7a1783d48d57e8e1b15b488c0eab1a00c556d597
jagathpraneshrameshbabu@gmail.com <jagathpraneshrameshbabu@gmail.com>
2025-02-14 21:00:39 +0530

    New Cards Component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,27 +1,22 @@
 import React from "react";
-import './Cards.css'
+import "./Cards.css"
 
-function Cards({ photo, title, details }) {
+const Cards = ({ suit, rank, image, name, description }) => {
   return (
-    <div className="event-card">
-      <section className="card-top">
-        K<br></br>♥
-      </section>
-      <div className="event-card-inside">
-        <section className="event-top">
-          <img src={photo} alt={"event-logo-" + title}></img>
-        </section>
-        <section className="event-details">
-          <h5>{title}</h5>
-          <p>{details}</p>
-          <button>Explore</button>
-        </section>
+    <div className="card">
+      <div className="card-corner top-left">
+        <span>{rank}</span>
+        <span>{suit}</span>
       </div>
-      <section className="card-bottom">
-        ♥<br></br>K
-      </section>
+      <img src={image} alt={name} className="event-image" />
+      <h2 className="event-name">{name}</h2>
+      <p className="event-description">{description}</p>
+      <div className="card-corner bottom-right">
+        <span>{rank}</span>
+        <span>{suit}</span>
+      </div>
     </div>
   );
-}
+};
 
 export default Cards;
--- src/components/Technical.jsx
+++ src/components/Technical.jsx
@@ -1,32 +1,9 @@
 import Navbar from "./Navbar";
 import './Technical.css'
-import king from "../assets/king.svg";
-import queen from "../assets/queen.svg";
 import Cards from "./Cards";
+import events from "./event.js"
 
 export default function Technical(){
-
-  const data = [
-    {
-     
-      eventName: "Event 1",
-      photo : king,
-      details:"event 1 details",
-    },
-    {
-      
-      eventName : "event 2",
-      photo : queen,
-      details:
-        "Event 2 details"
-    },
-    {
-      
-      eventName : "event 3",
-      photo : king,
-      details : "Event 3 Details"
-    }
-  ];
 
   return(
     <div>
@@ -36,10 +13,20 @@ export default function Technical(){
           <h1>Technical</h1>
         </div>
       </div>
-        <div className="events">
-          {data.map((event, index) => (
-              <Cards key={index} photo={event.photo} title={event.eventName} details={event.details} />
-          ))}
+        <div className="app">
+          {
+            events.map(event => (
+              <Cards
+                key={event.id}
+                suit={event.suit}
+                rank={event.rank}
+                image={event.image}
+                name={event.name}
+                description={event.description}
+              />
+            )
+          )
+          }
         </div>
     </div>
   )
